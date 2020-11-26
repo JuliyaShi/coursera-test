8536cc4fc6c0e99b8e7433b5d3749ad2765a8687
JuliyaShi <jvshirobokova@gmail.com>
2020-11-26 22:15:28 +0100

    edit3

--- mod4_solution/script.js
+++ mod4_solution/script.js
@@ -58,11 +58,13 @@ for (var i = 0; i < names.length; i++) {
   // 'j'. If the same, call byeSpeaker's 'speak' method with the current name
   // in the loop. Otherwise, call helloSpeaker's 'speak' method with the current
   // name in the loop.
-  if (firstLetter === 'j') {
-    byeSpeaker.speak(names[i]);
+  if (firstLetter == 'j') {
+    window.byeSpeaker.speak(names[i]);
   } else {
-    helloSpeaker.speak(names[i]);
+    window.helloSpeaker.speak(names[i]);
   }
 }
 
 })(window);
+
+
