e603e68b1d3705d9e902f497c0869e02843903f9
JuliyaShi <jvshirobokova@gmail.com>
2020-11-28 09:50:25 +0100

    final edit 28.11.2020

--- mod4_solution/SpeakHello.js
+++ mod4_solution/SpeakHello.js
@@ -7,7 +7,7 @@
 // See Lecture 52, part 1
  (function (window) {
  
- var helloSpeaker ={};
+ var helloSpeaker = {};
 
 // DO NOT attach the speakWord variable to the 'helloSpeaker' object.
 var speakWord = "Hello";
@@ -15,7 +15,7 @@ var speakWord = "Hello";
 // STEP 4: Rewrite the 'speak' function such that it is attached to the
 // helloSpeaker object instead of being a standalone function.
 // See Lecture 52, part 2
-helloSpeaker.speak(name)= function () {
+helloSpeaker.speak= function (name) {
   console.log(speakWord + " " + name);
 }
 
